dee8d18a4f7158464112d8200a99e6254ae56e51
agustin <agustinsanchezx@gmail.com>
2023-02-16 18:19:48 -0300

    add project Mouse Follower with React

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  const handleChange = () => {
+    setEnabled(!enabled)
+  }
+
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // Limpiar efecto (suscripcion)
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  }, [enabled])
+
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={handleChange}>
+        {enabled ? 'Unfollow' : 'Follow'} Mouse
+      </button>
+
+
+    </>
+  )
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  )
+}
+
+export default App
